408c1a44960929b3487e9af73fcf9697c5b5f446
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-10-22 16:35:04 +0300

    DONE

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    # The  setter method which take an argument and also  sets the instance variable
+    def name=(dog_name)
+        # all instance variables are bound to an instance
+        @this_dogs_name = dog_name
+   end
+#   return the value of the instance variable
+   def name
+    @this_dogs_name
+  end
 end
+name_dog = Dog.new
+name_dog.name = "rose"
+puts name_dog.name
